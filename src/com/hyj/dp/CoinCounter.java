c55994d79a4b685fe594b83999262918898de200
huangyingjie <huangyingjie@meituan.com>
2014-09-17 11:06:37 +0800

    refactor

--- src/com/hyj/dp/CoinCounter.java
+++ src/com/hyj/dp/CoinCounter.java
@@ -7,11 +7,9 @@ public class CoinCounter {
     public Integer getPlan(Integer total) {
         Integer[] coins = new Integer[] {1,3,5};
         Integer[] plan = new Integer[total+1];
-        for(int i = 0; i <= total; i++) {
+        plan[0] = 0;
+        for(int i = 1; i <= total; i++) {
             plan[i] = Integer.MAX_VALUE;
-            if (i == 0) {
-                plan[i] = 0;
-            }
             for (int coin : coins) {
                 if (coin <= i && plan[i] > plan[i - coin] + 1) {
                     plan[i] = plan[i - coin] + 1;
